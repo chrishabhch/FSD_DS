cd08298a4001fd183b9e65911c4e3477c526b9c0
chrishabhch <rishabhchaudhary914@gmail.com>
2024-10-21 09:45:35 +0530

    DAY7

--- DAY6/script.js
+++ DAY6/script.js
@@ -6,3 +6,20 @@ console.log(even);
 
 // let sum = arr.reduce((x,y)=>x+y,0);
 // console.log(sum);
+
+const students = [
+    { name: 'Rishabh' ,score:50 },
+    { name: 'Agrawal' ,score:45 },
+    { name: 'Harsh' ,score:46 },
+    { name: 'Kishan' ,score:56 }
+ ];
+ 
+ let newar = students
+ .filter((student) => student.score > 60)
+ .map((students) => students.score*2)
+ .reduce((x,y) => x+y,0);
+ console.log(newar);
+
+//  let newar2 = students.map((students) => students.score*2)
+//  console.log(newar2)
+
---
+++ DAY7/script.js
@@ -0,0 +1,21 @@
+// sayhello = () => {
+//     console.log("i am in hello function")
+// };
+
+// console.log("task started");
+// setTimeout(sayhello , 3000)
+// sayhello();
+// console.log("task end")
+
+
+console.log('start');
+setTimeout(() => {
+    console.log("first task completed");
+    setTimeout(() => {
+        console.log("second task completed");
+        setTimeout(() => {
+            console.log('third task completed')
+        },3000);
+    },2000);
+},1000);
+console.log("End")
